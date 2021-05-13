ae5068161bbf974541e9d47246e7ff95f32dee8c
Avinesh <avineshgupta999@gmail.com>
2021-05-13 11:50:18 +0530

    2nd commit

---
+++ src/screens/SignupScreen.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
+
+const SignupScreen = ({ navigation }) => {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	return (
+		<View style={styles.container}>
+			<Spacer>
+				<Text h3>Sign up for Tracker</Text>
+			</Spacer>
+			<Input
+				label="Email"
+				value={email}
+				onChangeText={setEmail}
+				autoCapitalize="none"
+				autoCorrect={false}
+			/>
+			<Spacer />
+			<Input
+				secureTextEntry
+				label="Password"
+				value={password}
+				onChangeText={setPassword}
+				autoCapitalize="none"
+				autoCorrect={false}
+			/>
+			<Spacer>
+				<Button title="Sign Up" />
+			</Spacer>
+		</View>
+	);
+};
+
+SignupScreen.navigationOptions = () => {
+	return {
+		headerShown: false,
+	};
+};
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1, // so that parent view fills as much space on screen as it can
+		justifyContent: "center",
+		marginBottom: 200,
+	},
+});
+
+export default SignupScreen;
